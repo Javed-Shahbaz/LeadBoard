ae8c1ebb75b3825269a5f8256e7ee25da6527a62
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-20 09:57:04 +0500

    update files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+const scoreList = [
+  {
+    id: '1',
+    name: 'Name',
+    score: '100',
+  },
+  {
+    id: '2',
+    name: 'Name',
+    score: '20',
+  },
+  {
+    id: '3',
+    name: 'Name',
+    score: '50',
+  },
+  {
+    id: '4',
+    name: 'Name',
+    score: '78',
+  },
+  {
+    id: '5',
+    name: 'Name',
+    score: '125',
+  },
+];
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+const lisItem = document.getElementById('List');
 
-   return element;
- }
-
- document.body.appendChild(component());
+scoreList.map((item) => {
+  if (item.id % 2 === 0) {
+    lisItem.innerHTML += `
+    <li class= 'bakground'>${item.name}:     <span>${item.score}</span></li>`;
+  } else {
+    lisItem.innerHTML += `<li>${item.name}:     <span>${item.score}</span></li>`;
+  }
+  return ("");
+});
