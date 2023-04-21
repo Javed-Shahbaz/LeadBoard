fb3fd7df9108b7a9b17304e0bdb13f81e98a2a17
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-21 11:11:18 +0500

    create class

---
+++ src/Modules/apiData.js
@@ -0,0 +1,11 @@
+class scoreBoard {
+  constructor(name, score) {
+    this.user = user;
+    this.score = score;
+  }
+
+  //  stores data in array
+  scoresData = [];
+
+}
+export default scoreBoard
