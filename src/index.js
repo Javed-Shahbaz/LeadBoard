4dc74d2b77b6863cc0fa708a603d06f900bec204
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-21 10:56:53 +0500

    update changes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
 import './style.css';
 
-const scoreList = [
-  {
-    id: '1',
-    name: 'Name',
-    score: '100',
-  },
-  {
-    id: '2',
-    name: 'Name',
-    score: '20',
-  },
-  {
-    id: '3',
-    name: 'Name',
-    score: '50',
-  },
-  {
-    id: '4',
-    name: 'Name',
-    score: '78',
-  },
-  {
-    id: '5',
-    name: 'Name',
-    score: '125',
-  },
-];
-
 const lisItem = document.getElementById('List');
 
 scoreList.map((item) => {
